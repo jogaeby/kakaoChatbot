6e8afc73c18127d3f8596fbfc2c8bda698036be7
한수빈 <vinsulill@gmail.com>
2025-04-17 11:18:43 +0900

    init

--- Bproject/src/main/java/com/chatbot/base/dto/kakao/constatnt/ChatStatus.java
+++
@@ -1,5 +0,0 @@
-package com.chatbot.base.dto.kakao.constatnt;
-
-public enum ChatStatus {
-    요청,응답대기,응답완료
-}
--- Bproject/src/main/java/com/chatbot/base/dto/kakao/constatnt/button/ButtonParamKey.java
+++
@@ -1,9 +0,0 @@
-package com.chatbot.base.dto.kakao.constatnt.button;
-
-public enum ButtonParamKey {
-    productId,choice,noticeId,productStatus,orderId,pageNumber,firstNumber,lastNumber,searchWord
-
-    /**
-     *  key 추가시 ClientExtra 클래스의 맴버변수 추가해야함.
-     */
-}
