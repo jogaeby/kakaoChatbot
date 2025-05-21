f49f953c59e258a02a55af869f333f6f3f98d1ce
한수빈 <vinsulill@gmail.com>
2025-05-21 16:38:12 +0900

    feat: 문구수정

--- src/main/java/com/chatbot/base/view/KakaoChatBotView.java
+++ src/main/java/com/chatbot/base/view/KakaoChatBotView.java
@@ -16,8 +16,7 @@ public class KakaoChatBotView {
         message
                 .append("이벤트 응모 정보 접수를 위해")
                 .append("\n")
-                .append("카카오싱크 연동이 필요해요,")
-                .append("\n")
+                .append("카카오싱크 연동이 필요해요.")
                 .append("\n")
                 .append("계속 진행하시겠어요?")
         ;
