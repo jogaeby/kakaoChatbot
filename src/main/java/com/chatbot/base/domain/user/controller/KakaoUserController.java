c0f64987a92c700a9a95fe1932b7e11574810eb7
한수빈 <vinsulill@gmail.com>
2025-10-28 09:29:03 +0900

    feat: 환불계좌

--- src/main/java/com/chatbot/base/domain/user/controller/KakaoUserController.java
+++ src/main/java/com/chatbot/base/domain/user/controller/KakaoUserController.java
@@ -156,6 +156,7 @@ public class KakaoUserController {
                 carousel.addComponent(accountInfoItemCard);
 
                 chatBotResponse.addCarousel(carousel);
+                StringFormatterUtil.objectToString(chatBotResponse);
                 return chatBotResponse;
             }
 
